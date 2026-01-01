bd81a99f98aaac969360462ae9b950897e3cde4b
sandali <sandaliwathsalavi25@gmail.com>
2026-01-01 23:58:31 +0530

    compare password method

--- backend/models/User.js
+++ backend/models/User.js
@@ -41,7 +41,7 @@ userSchema.pre('save', async function(next) {
   }
 });
 
-// Compare password method
+ //Compare password method
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return await bcrypt.compare(candidatePassword, this.password);
 };
